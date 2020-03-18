7d21b1ac3f2b25c78a8a12f8b97eca390154ece1
jakeh <jakeh@10.182.198.187>
2020-03-18 20:38:15 +0000

    system class

---
+++ depotsystem/src/depotsystem/menusystem.java
@@ -0,0 +1,11 @@
+package depotsystem;
+
+public class menusystem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is the depot system.");
+	}
+
+}
