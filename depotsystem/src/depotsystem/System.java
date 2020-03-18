50f3c4213dbf861132636d83803bb06a99fe30a9
jakeh <jakeh@10.182.198.187>
2020-03-18 20:12:20 +0000

    class

---
+++ depotsystem/src/depotsystem/System.java
@@ -0,0 +1,14 @@
+package depotsystem;
+
+public class System {
+
+	public System() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
