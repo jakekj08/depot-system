f0f91fbd191e4cbc9e9a09393bdc3f7060c98b29
jakeh <jakeh@10.182.198.187>
2020-03-18 20:40:44 +0000

    all classes

---
+++ depotsystem/src/depotsystem/Tanker.java
@@ -0,0 +1,10 @@
+package depotsystem;
+
+public class Tanker {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ depotsystem/src/depotsystem/WorkSchedule.java
@@ -0,0 +1,10 @@
+package depotsystem;
+
+public class WorkSchedule {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
