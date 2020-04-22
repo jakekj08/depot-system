d34ac0a73aae1054f68830c162997d3c5506fa3d
Snuffle <Snuffle@192.168.0.24>
2020-04-23 00:03:27 +0100

    cum4

--- depotsystem/src/depotsystem/MenuSystem.java
+++ depotsystem/src/depotsystem/MenuSystem.java
@@ -9,18 +9,18 @@ public class MenuSystem {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.printf("- - Welcome to the e-Depot System - -%n");
+		System.out.printf("-//- Welcome to the E-Depot System -//- -%n");
         String choice = "";
         
         
         do {
-        System.out.printf("- - Menu - -%n");
-        System.out.printf("1 - View Work Schedule -%n");
-        System.out.printf("2 - Setup Work Schedule -%n");
-        System.out.printf("3 - Move Vehicle -%n");
-        System.out.printf("4 - Option 4 -%n");
-        System.out.printf("E - Exit System -%n");
-        System.out.printf("Pick:  %n");
+        System.out.printf("-//- Menu -//- -%n");
+        System.out.printf("1 -||- View Work Schedule  -||- -%n");
+        System.out.printf("2 -||- Setup Work Schedule -||- -%n");
+        System.out.printf("3 -||- Move Vehicle		  -||- -%n");
+        System.out.printf("4 -||- Option 4            -||- -%n");
+        System.out.printf("E -||- Exit System         -||- -%n");
+        System.out.printf("Choose:  %n");
         
         
         choice = S.next().toUpperCase();
@@ -51,8 +51,8 @@ public class MenuSystem {
         }
     
     while (!choice.equals("E"));
-        System.out.println("- -System Exit- -");
-        System.out.println("     Goodbye ");
+        System.out.println("-||- System Exit -||-");
+        System.out.println("     ~||~ Goodbye ~||~ ");
     }
     
     public void GetDepot() {
