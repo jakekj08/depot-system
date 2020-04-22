c2c88eb03f435c7d8ca375f973918f7ab958d743
Snuffle <Snuffle@192.168.0.24>
2020-04-22 23:15:53 +0100

    cum

--- depotsystem/src/depotsystem/Depot.java
+++ depotsystem/src/depotsystem/Depot.java
@@ -3,10 +3,17 @@ import java.util.Scanner;
 
 
 public class Depot {
-    
-    
-    public void logOn(String username, String password) {
-        
+public void logon() {	
+    	Scanner scan = new Scanner (new File ("the\\dir\\Depot.java"));
+    	Scanner input = new Scanner (System.in);
+    	  System.out.printf("");
+    	  System.out.printf("Please enter your UserName: ");
+    	  String username = scan.nextLine();
+    	    System.out.printf("Please enter your Password: ");
+    	    String password = scan.nextLine();
+    	    
+    	    String Inputusername = input.nextLine();
+    	    String InputPass = input.nextLine();
     }
     
        public Vehicle GetVehicle() {
