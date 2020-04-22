1b00311cabac24585aa1fd292daca81ac915cdd4
jakeh <jakeh@10.182.198.187>
2020-04-22 22:27:48 +0100

    d

--- depotsystem/src/depotsystem/Depot.java
+++ depotsystem/src/depotsystem/Depot.java
@@ -1,10 +1,26 @@
 package depotsystem;
+import java.util.Scanner;
+
 
 public class Depot {
 
-	public static void main(String[] args) {
+	public static void LogOn(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		
+		String email;
+		String password;
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Hello, please log onto the system.");
+		System.out.println("Email :");
+		
+		email = scan.nextLine();
+		
+		System.out.println("Password :");
+		
+		
 	}
 
 }
