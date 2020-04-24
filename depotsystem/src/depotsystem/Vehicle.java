e0f5a84b39c5802dcf5965711f7c08782daf03a2
Snuffle <Snuffle@192.168.0.24>
2020-04-24 17:15:45 +0100

    fat COCK

--- depotsystem/src/depotsystem/Vehicle.java
+++ depotsystem/src/depotsystem/Vehicle.java
@@ -2,13 +2,13 @@ package depotsystem;
 
 public class Vehicle extends MenuSystem {
     private String[] make;
-    protected String model;
-    protected String regNo;
-    protected int weight;
+    private String[] model;
+    private String[] regNo;
+    private int[] weight;
 
       
 
-    public void Vehicle(String[] make, String model, String regNo, int weight) {
+    public Vehicle(String[] make, String[] model, String[] regNo, int[] weight) {
         this.make = make;
         this.model = model;
         this.regNo = regNo;
@@ -21,6 +21,15 @@ public class Vehicle extends MenuSystem {
     
     String[] CarMake = new String[] {"Audi", "BMW", "Ford", "Mazda"};
     CarMake = make;
+    
+    String[] CarModel = new String[] {"Alpha", "SportGT", "Alexa", "HaulMaster"};
+    CarModel = model;
+    
+      String[] CarReg = new String[] {"IHZ 5734", "GJZ 9021", "PLZ 9727", "RKZ 1638"};
+    CarReg = regNo;
+    
+      int[] CarWeight = new int[] {130, 1542, 1287, 1765};
+    CarWeight = weight;
     
     System.out.println(CarMake.toString());
     }
