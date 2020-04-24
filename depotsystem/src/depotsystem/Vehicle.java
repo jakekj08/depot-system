8f548e4ae296b87118908c7105114e8efda78ddc
Snuffle <Snuffle@192.168.0.24>
2020-04-24 20:02:19 +0100

    pog

--- depotsystem/src/depotsystem/Vehicle.java
+++ depotsystem/src/depotsystem/Vehicle.java
@@ -5,7 +5,7 @@ public class Vehicle extends MenuSystem {
     private String[] model;
     private String[] regNo;
     private int[] weight;
-
+    //Defining data types
       
 
     public Vehicle(String[] make, String[] model, String[] regNo, int[] weight) {
@@ -17,22 +17,25 @@ public class Vehicle extends MenuSystem {
     
     }
     public Truck[] VehicleData; {
-    VehicleData = (Truck[]) new Vehicle[2];
+    VehicleData = (Truck[]) new Vehicle[4];
     
-    String[] CarMake = new String[] {"Audi", "BMW", "Ford", "Mazda"};
+    String[] CarMake = new String[] {"Audi", "BMW", "Ford", "Mazda"};	
     CarMake = make;
+    //Array of different car makes
     
     String[] CarModel = new String[] {"Alpha", "SportGT", "Alexa", "HaulMaster"};
     CarModel = model;
-    
+    //Array of different car models
       String[] CarReg = new String[] {"IHZ 5734", "GJZ 9021", "PLZ 9727", "RKZ 1638"};
     CarReg = regNo;
-    
+    //Array of stored car registration numbers
       int[] CarWeight = new int[] {130, 1542, 1287, 1765};
     CarWeight = weight;
+    //Array of different weights of cars
     
     System.out.println(CarMake.toString());
     }
+    //Print to check if data is correctly stored
     
     public boolean IsAvailable() { 
         return false; 
