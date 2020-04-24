d8922a498a8b0c522da73605df2ab7fe30987a9d
jakeh <jakeh@10.182.198.187>
2020-04-24 01:23:54 +0100

    update

--- depotsystem/src/depotsystem/Tanker.java
+++ depotsystem/src/depotsystem/Tanker.java
@@ -5,7 +5,7 @@ public class Tanker extends Vehicle{
     private final int liquidCapacity;
     private final String liquidType;
     
-    public Tanker(String make,String model, String regNo, int weight, int liquidCapacity, String liquidType) {
+    public Tanker(String[] make,String model, String regNo, int weight, int liquidCapacity, String liquidType) {
         super(make, model, regNo, weight);
         this.liquidCapacity = liquidCapacity;
         this.liquidType = liquidType;
--- depotsystem/src/depotsystem/Vehicle.java
+++ depotsystem/src/depotsystem/Vehicle.java
@@ -1,25 +1,36 @@
 package depotsystem;
 
-public abstract class Vehicle {
-    protected String make;
+public class Vehicle extends MenuSystem {
+    private String[] make;
     protected String model;
     protected String regNo;
     protected int weight;
 
-    
-    public Vehicle(String make, String model, String regNo, int weight) {
+      
+
+    public Vehicle(String[] make, String model, String regNo, int weight) {
         this.make = make;
         this.model = model;
         this.regNo = regNo;
         this.weight = weight;
+
+    
+    }
+    public Vehicle[] VehicleData; {
+    VehicleData = new Vehicle[2];
+    
+    String[] CarMake = new String[] {"Audi", "BMW", "Ford", "Mazda"};
+    CarMake = make;
+    
+    System.out.println(CarMake.toString());
     }
     
     public boolean IsAvailable() { 
         return false; 
     }
-    
+
     public void SetSchedule() {
-        
+
     }
-    
+
 }
